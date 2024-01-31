24d4b03a71a4d02efc21a562eab692de3a0cb5b2
rezwan <rezwanshahriorrahin@gmail.com>
2024-01-31 19:50:55 +0600

    created a model class named 'Character'

---
+++ RPG/Models/Character.cs
@@ -0,0 +1,13 @@
+﻿namespace RPG.Models
+{
+	public class Character
+	{
+		public int Id { get; set; }
+		public string Name { get; set; } = "Frodo";
+		public int HitPoints { get; set; } = 100;
+		public int Strength { get; set; } = 10;
+		public int Defense { get; set; } = 10;
+		public int Intelligence { get; set; } = 10;
+		public RPGClass Class { get; set; } = RPGClass.Knight;
+	}
+}
