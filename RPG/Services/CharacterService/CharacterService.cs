a208a26a796f582ceba79dd080f7adc511a0ab7c
rezwan <rezwanshahriorrahin@gmail.com>
2024-02-01 17:18:57 +0600

    created a service class, implementing its interface, named 'CharacterService'

---
+++ RPG/Services/CharacterService/CharacterService.cs
@@ -0,0 +1,22 @@
+using RPG.Models;
+
+namespace RPG.Services.CharacterService
+{
+	public class CharacterService : ICharacterService
+	{
+		public List<Character> AddCharacter(Character newCharacter)
+		{
+			throw new NotImplementedException();
+		}
+
+		public List<Character> GetAllCharacters()
+		{
+			throw new NotImplementedException();
+		}
+
+		public Character GetCharacterById(int id)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
