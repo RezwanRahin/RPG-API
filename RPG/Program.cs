6b2d9e78edbfcf01ecc13313a439524c494bade3
rezwan <rezwanshahriorrahin@gmail.com>
2024-02-01 18:13:14 +0600

    registered 'ICharacterService' service in project Program file

--- RPG/Program.cs
+++ RPG/Program.cs
@@ -1,3 +1,5 @@
+using RPG.Services.CharacterService;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<ICharacterService, CharacterService>();
 
 var app = builder.Build();
 
