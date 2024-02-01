09e6548887be58bedb442688b19a0dbbd1d78f5e
rezwan <rezwanshahriorrahin@gmail.com>
2024-02-01 17:20:45 +0600

    added a static property to 'CharacterService'

--- RPG/Services/CharacterService/CharacterService.cs
+++ RPG/Services/CharacterService/CharacterService.cs
@@ -4,6 +4,11 @@ namespace RPG.Services.CharacterService
 {
 	public class CharacterService : ICharacterService
 	{
+		private static List<Character> characters = new List<Character> {
+			new Character(),
+			new Character { Id = 1, Name = "Sam" }
+		};
+
 		public List<Character> AddCharacter(Character newCharacter)
 		{
 			throw new NotImplementedException();
