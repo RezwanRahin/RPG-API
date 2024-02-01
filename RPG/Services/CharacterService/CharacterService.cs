a420f0a37f2e330ec6ce206cb692f6a00195350a
rezwan <rezwanshahriorrahin@gmail.com>
2024-02-01 18:50:38 +0600

    converted all the methods to asynchronous in 'CharacterService' class

--- RPG/Services/CharacterService/CharacterService.cs
+++ RPG/Services/CharacterService/CharacterService.cs
@@ -9,18 +9,18 @@ namespace RPG.Services.CharacterService
 			new Character { Id = 1, Name = "Sam" }
 		};
 
-		public List<Character> AddCharacter(Character newCharacter)
+		public async Task<List<Character>> AddCharacter(Character newCharacter)
 		{
 			characters.Add(newCharacter);
 			return characters;
 		}
 
-		public List<Character> GetAllCharacters()
+		public async Task<List<Character>> GetAllCharacters()
 		{
 			return characters;
 		}
 
-		public Character GetCharacterById(int id)
+		public async Task<Character> GetCharacterById(int id)
 		{
 			var character = characters.FirstOrDefault(c => c.Id == id);
 
