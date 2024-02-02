de79a7ec0c8c3bf1c984050daff35d06895d0d3b
rezwan <rezwanshahriorrahin@gmail.com>
2024-02-02 14:08:14 +0600

    registered 'AutoMapper' service in project Program file

--- RPG/Program.cs
+++ RPG/Program.cs
@@ -9,6 +9,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
+
 builder.Services.AddScoped<ICharacterService, CharacterService>();
 
 var app = builder.Build();
