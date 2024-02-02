d4dfff7d407b3e480b7b590d2a2af80f1ac8a722
rezwan <rezwanshahriorrahin@gmail.com>
2024-02-02 15:00:33 +0600

    adjusted all the methods, with Character DTOs replacing 'Character' model, of 'ICharacterService' interface

--- RPG/Services/CharacterService/ICharacterService.cs
+++ RPG/Services/CharacterService/ICharacterService.cs
@@ -1,11 +1,12 @@
+using RPG.Dtos.Character;
 using RPG.Models;
 
 namespace RPG.Services.CharacterService
 {
 	public interface ICharacterService
 	{
-		Task<ServiceResponse<List<Character>>> GetAllCharacters();
-		Task<ServiceResponse<Character>> GetCharacterById(int id);
-		Task<ServiceResponse<List<Character>>> AddCharacter(Character newCharacter);
+		Task<ServiceResponse<List<GetCharacterDto>>> GetAllCharacters();
+		Task<ServiceResponse<GetCharacterDto>> GetCharacterById(int id);
+		Task<ServiceResponse<List<GetCharacterDto>>> AddCharacter(AddCharacterDto newCharacter);
 	}
 }
