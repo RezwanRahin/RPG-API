fb4132a4e823ab2f4d309ed5b47c79bbcb4f2e45
rezwan <rezwanshahriorrahin@gmail.com>
2024-02-02 15:33:14 +0600

    adjusted all the methods, with Character DTOs replacing 'Character' model, of 'CharacterController'

--- RPG/Controllers/CharacterController.cs
+++ RPG/Controllers/CharacterController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using RPG.Dtos.Character;
 using RPG.Models;
 using RPG.Services.CharacterService;
 
@@ -16,19 +17,19 @@ namespace RPG.Controllers
 		}
 
 		[HttpGet("GetAll")]
-		public async Task<ActionResult<ServiceResponse<List<Character>>>> Get()
+		public async Task<ActionResult<ServiceResponse<List<GetCharacterDto>>>> Get()
 		{
 			return Ok(await _characterService.GetAllCharacters());
 		}
 
 		[HttpGet("{id}")]
-		public async Task<ActionResult<ServiceResponse<Character>>> GetSingle(int id)
+		public async Task<ActionResult<ServiceResponse<GetCharacterDto>>> GetSingle(int id)
 		{
 			return Ok(await _characterService.GetCharacterById(id));
 		}
 
 		[HttpPost]
-		public async Task<ActionResult<ServiceResponse<List<Character>>>> AddCharacter(Character newCharacter)
+		public async Task<ActionResult<ServiceResponse<List<GetCharacterDto>>>> AddCharacter(AddCharacterDto newCharacter)
 		{
 			return Ok(await _characterService.AddCharacter(newCharacter));
 		}
