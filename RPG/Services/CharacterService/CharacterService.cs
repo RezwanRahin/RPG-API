b435f858d2de40983be0396e4245c75c0a40a6be
rezwan <rezwanshahriorrahin@gmail.com>
2024-02-02 15:08:18 +0600

    modified the 'AddCharacter' action method, adjusting with Character DTOs replacing 'Character' model, of 'CharacterService' class

--- RPG/Services/CharacterService/CharacterService.cs
+++ RPG/Services/CharacterService/CharacterService.cs
@@ -1,4 +1,5 @@
 using AutoMapper;
+using RPG.Dtos.Character;
 using RPG.Models;
 
 namespace RPG.Services.CharacterService
@@ -17,11 +18,13 @@ namespace RPG.Services.CharacterService
 			_mapper = mapper;
 		}
 
-		public async Task<ServiceResponse<List<Character>>> AddCharacter(Character newCharacter)
+		public async Task<ServiceResponse<List<GetCharacterDto>>> AddCharacter(AddCharacterDto newCharacter)
 		{
-			var serviceResponse = new ServiceResponse<List<Character>>();
-			characters.Add(newCharacter);
-			serviceResponse.Data = characters;
+			var serviceResponse = new ServiceResponse<List<GetCharacterDto>>();
+			var character = _mapper.Map<Character>(newCharacter);
+			character.Id = characters.Max(c => c.Id) + 1;
+			characters.Add(character);
+			serviceResponse.Data = characters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToList();
 			return serviceResponse;
 		}
 
