84b161142e642bfbffccc6fd5fa3da274049107e
rezwan <rezwanshahriorrahin@gmail.com>
2024-02-03 23:29:44 +0600

    added a method named 'DeleteCharacter' to 'ICharacterService' interface

--- RPG/Services/CharacterService/ICharacterService.cs
+++ RPG/Services/CharacterService/ICharacterService.cs
@@ -9,5 +9,6 @@ namespace RPG.Services.CharacterService
 		Task<ServiceResponse<GetCharacterDto>> GetCharacterById(int id);
 		Task<ServiceResponse<List<GetCharacterDto>>> AddCharacter(AddCharacterDto newCharacter);
 		Task<ServiceResponse<GetCharacterDto>> UpdateCharacter(UpdateCharacterDto updatedCharacter);
+		Task<ServiceResponse<List<GetCharacterDto>>> DeleteCharacter(int id);
 	}
 }
