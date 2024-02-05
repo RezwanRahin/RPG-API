dabce5adc03fde73ec4c660d2c1449dde3f0f02f
rezwan <rezwanshahriorrahin@gmail.com>
2024-02-05 14:49:45 +0600

    set 1-to-Many relationship between 'User' & 'Character' classes

--- RPG/Models/Character.cs
+++ RPG/Models/Character.cs
@@ -9,5 +9,6 @@
 		public int Defense { get; set; } = 10;
 		public int Intelligence { get; set; } = 10;
 		public RPGClass Class { get; set; } = RPGClass.Knight;
+		public User? User { get; set; }
 	}
 }
