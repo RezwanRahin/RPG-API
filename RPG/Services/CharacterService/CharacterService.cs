214f053271724c9b629f6e99d9cea2908cdc3a96
rezwan <rezwanshahriorrahin@gmail.com>
2024-02-05 14:17:44 +0600

    modified 'DeleteCharacter' action method, to adjust with 'DataContext', of 'CharacterService' class

--- RPG/Services/CharacterService/CharacterService.cs
+++ RPG/Services/CharacterService/CharacterService.cs
@@ -40,13 +40,15 @@ namespace RPG.Services.CharacterService
 
 			try
 			{
-				var character = characters.FirstOrDefault(c => c.Id == id);
+				var character = await _context.Characters.FirstOrDefaultAsync(c => c.Id == id);
 				if (character is null)
 					throw new Exception($"Character with Id '{id}' not found.");
 
-				characters.Remove(character);
+				_context.Characters.Remove(character);
 
-				serviceResponse.Data = characters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToList();
+				await _context.SaveChangesAsync();
+
+				serviceResponse.Data = await _context.Characters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToListAsync();
 			}
 			catch (Exception ex)
 			{
