a9e56616bea2eabfee279592de6342da8742da76
rezwan <rezwanshahriorrahin@gmail.com>
2024-02-06 21:13:24 +0600

    added a private method named 'VerifyPasswordHash' to 'AuthRepository' class

--- RPG/Data/AuthRepository.cs
+++ RPG/Data/AuthRepository.cs
@@ -55,5 +55,14 @@ namespace RPG.Data
 				passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
 			}
 		}
+
+		private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+		{
+			using (var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
+			{
+				var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+				return computedHash.SequenceEqual(passwordHash);
+			}
+		}
 	}
 }
