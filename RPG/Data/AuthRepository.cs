f655c53d0816a0b7046f9d26055c9abf6f24ede6
rezwan <rezwanshahriorrahin@gmail.com>
2024-02-06 12:59:04 +0600

    created a repository class, implementing its interface, named 'AuthRepository'

---
+++ RPG/Data/AuthRepository.cs
@@ -0,0 +1,22 @@
+using RPG.Models;
+
+namespace RPG.Data
+{
+	public class AuthRepository : IAuthRepository
+	{
+		public Task<ServiceResponse<string>> Login(string username, string password)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<ServiceResponse<int>> Register(User user, string password)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<bool> UserExists(string username)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
