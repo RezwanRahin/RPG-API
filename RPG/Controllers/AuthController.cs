53ea50928af6f32b8499ced65b41fe4572998793
rezwan <rezwanshahriorrahin@gmail.com>
2024-02-06 21:25:13 +0600

    added a HttpPost action method named 'Login' to 'AuthController'

--- RPG/Controllers/AuthController.cs
+++ RPG/Controllers/AuthController.cs
@@ -29,5 +29,17 @@ namespace RPG.Controllers
 			}
 			return Ok(response);
 		}
+
+		[HttpPost("Login")]
+		public async Task<ActionResult<ServiceResponse<int>>> Login(UserRegisterDto request)
+		{
+			var response = await _authRepo.Login(request.Username, request.Password);
+
+			if (!response.Success)
+			{
+				return BadRequest(response);
+			}
+			return Ok(response);
+		}
 	}
 }
