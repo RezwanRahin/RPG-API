f0d91ad62fb8f91b28540eb361effbc6d03777eb
rezwan <rezwanshahriorrahin@gmail.com>
2024-02-06 13:20:07 +0600

    provided implementation of 'Register' method of 'AuthRepository' class

--- RPG/Data/AuthRepository.cs
+++ RPG/Data/AuthRepository.cs
@@ -16,9 +16,18 @@ namespace RPG.Data
 			throw new NotImplementedException();
 		}
 
-		public Task<ServiceResponse<int>> Register(User user, string password)
+		public async Task<ServiceResponse<int>> Register(User user, string password)
 		{
-			throw new NotImplementedException();
+			CreatePasswordHash(password, out byte[] passwordHash, out byte[] passwordSalt);
+
+			user.PasswordHash = passwordHash;
+			user.PasswordSalt = passwordSalt;
+
+			_context.Users.Add(user);
+			await _context.SaveChangesAsync();
+			var response = new ServiceResponse<int>();
+			response.Data = user.Id;
+			return response;
 		}
 
 		public Task<bool> UserExists(string username)
