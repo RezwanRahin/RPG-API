60305f93076e739a101f63c76cec2995b3c238a6
rezwan <rezwanshahriorrahin@gmail.com>
2024-02-06 12:56:59 +0600

    created a repository interface named 'IAuthRepository'

---
+++ RPG/Data/IAuthRepository.cs
@@ -0,0 +1,11 @@
+using RPG.Models;
+
+namespace RPG.Data
+{
+	public interface IAuthRepository
+	{
+		Task<ServiceResponse<int>> Register(User user, string password);
+		Task<ServiceResponse<string>> Login(string username, string password);
+		Task<bool> UserExists(string username);
+	}
+}
