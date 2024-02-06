00ff4a9f5a2203a8604186fa0690b17103be7912
rezwan <rezwanshahriorrahin@gmail.com>
2024-02-06 13:18:07 +0600

    added a private method named 'CreatePasswordHash' to 'AuthRepository' class

--- RPG/Data/AuthRepository.cs
+++ RPG/Data/AuthRepository.cs
@@ -25,5 +25,14 @@ namespace RPG.Data
 		{
 			throw new NotImplementedException();
 		}
+
+		private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+		{
+			using (var hmac = new System.Security.Cryptography.HMACSHA512())
+			{
+				passwordSalt = hmac.Key;
+				passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+			}
+		}
 	}
 }
