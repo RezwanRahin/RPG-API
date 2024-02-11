53bd6285a062cc04336a4ece84dc91ad3bb40dc0
rezwan <rezwanshahriorrahin@gmail.com>
2024-02-11 19:18:28 +0600

    added 'Authorize' attribute to 'CharacterController' class

--- RPG/Controllers/CharacterController.cs
+++ RPG/Controllers/CharacterController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using RPG.Dtos.Character;
 using RPG.Models;
@@ -6,6 +7,7 @@ using RPG.Services.CharacterService;
 namespace RPG.Controllers
 {
 	[ApiController]
+	[Authorize]
 	[Route("api/[controller]")]
 	public class CharacterController : ControllerBase
 	{
