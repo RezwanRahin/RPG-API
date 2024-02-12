e4b91d3aefb119a399d6cd7561d24fcce0f5975d
rezwan <rezwanshahriorrahin@gmail.com>
2024-02-12 19:44:49 +0600

    reverted to no parameter of 'GetAllCharacters' method of 'CharacterService'

--- RPG/Services/CharacterService/ICharacterService.cs
+++ RPG/Services/CharacterService/ICharacterService.cs
@@ -5,7 +5,7 @@ namespace RPG.Services.CharacterService
 {
 	public interface ICharacterService
 	{
-		Task<ServiceResponse<List<GetCharacterDto>>> GetAllCharacters(int userId);
+		Task<ServiceResponse<List<GetCharacterDto>>> GetAllCharacters();
 		Task<ServiceResponse<GetCharacterDto>> GetCharacterById(int id);
 		Task<ServiceResponse<List<GetCharacterDto>>> AddCharacter(AddCharacterDto newCharacter);
 		Task<ServiceResponse<GetCharacterDto>> UpdateCharacter(UpdateCharacterDto updatedCharacter);
