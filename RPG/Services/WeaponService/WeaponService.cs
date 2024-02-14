15babff1141e8c3f445879144677f0bdd6beecba
rezwan <rezwanshahriorrahin@gmail.com>
2024-02-14 18:39:40 +0600

    added a constructor, injecting 'DataContext', ''IHttpContextAccessor, and 'IMapper' services, to 'WeaponService' class

--- RPG/Services/WeaponService/WeaponService.cs
+++ RPG/Services/WeaponService/WeaponService.cs
@@ -1,4 +1,6 @@
-﻿using RPG.Dtos.Character;
+﻿using AutoMapper;
+using RPG.Data;
+using RPG.Dtos.Character;
 using RPG.Dtos.Weapon;
 using RPG.Models;
 
@@ -6,6 +8,17 @@ namespace RPG.Services.WeaponService
 {
 	public class WeaponService : IWeaponService
 	{
+		private readonly DataContext _context;
+		private readonly IHttpContextAccessor _httpContextAccessor;
+		private readonly IMapper _mapper;
+
+		public WeaponService(DataContext context, IHttpContextAccessor httpContextAccessor, IMapper mapper)
+		{
+			_context = context;
+			_httpContextAccessor = httpContextAccessor;
+			_mapper = mapper;
+		}
+
 		public Task<ServiceResponse<GetCharacterDto>> AddWeapon(AddWeaponDto newWeapon)
 		{
 			throw new NotImplementedException();
