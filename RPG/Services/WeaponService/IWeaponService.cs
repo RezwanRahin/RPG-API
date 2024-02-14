2828a033a80e3149745d86bd2768630833b63fab
rezwan <rezwanshahriorrahin@gmail.com>
2024-02-14 15:22:25 +0600

    created a service interface named 'IWeaponService'

---
+++ RPG/Services/WeaponService/IWeaponService.cs
@@ -0,0 +1,11 @@
+﻿using RPG.Dtos.Character;
+using RPG.Dtos.Weapon;
+using RPG.Models;
+
+namespace RPG.Services.WeaponService
+{
+	public interface IWeaponService
+	{
+		Task<ServiceResponse<GetCharacterDto>> AddWeapon(AddWeaponDto newWeapon);
+	}
+}
