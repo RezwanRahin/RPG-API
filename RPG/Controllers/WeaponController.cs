f8930f04d199a6a904e4f5e97aee85edff95c38c
rezwan <rezwanshahriorrahin@gmail.com>
2024-02-14 19:14:44 +0600

    added 'Authorize' attribute to 'WeaponController' class

--- RPG/Controllers/WeaponController.cs
+++ RPG/Controllers/WeaponController.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace RPG.Controllers
 {
 	[Route("api/[controller]")]
+	[Authorize]
 	[ApiController]
 	public class WeaponController : ControllerBase
 	{
