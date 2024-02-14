e13c10247657068bc728318fc44fa3654a9f8f00
rezwan <rezwanshahriorrahin@gmail.com>
2024-02-14 15:24:25 +0600

    created a class, implementing its interface, named 'WeaponService'

---
+++ RPG/Services/WeaponService/WeaponService.cs
@@ -0,0 +1,14 @@
+﻿using RPG.Dtos.Character;
+using RPG.Dtos.Weapon;
+using RPG.Models;
+
+namespace RPG.Services.WeaponService
+{
+	public class WeaponService : IWeaponService
+	{
+		public Task<ServiceResponse<GetCharacterDto>> AddWeapon(AddWeaponDto newWeapon)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
