f325b186f9748d5c2e9d1f3667b81d3cb10eda81
rezwan <rezwanshahriorrahin@gmail.com>
2024-02-14 19:10:34 +0600

    provided implementation of 'AddWeapon' method of 'WeaponService' class

--- RPG/Services/WeaponService/WeaponService.cs
+++ RPG/Services/WeaponService/WeaponService.cs
@@ -1,4 +1,6 @@
-﻿using AutoMapper;
+﻿using System.Security.Claims;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
 using RPG.Data;
 using RPG.Dtos.Character;
 using RPG.Dtos.Weapon;
@@ -19,9 +21,42 @@ namespace RPG.Services.WeaponService
 			_mapper = mapper;
 		}
 
-		public Task<ServiceResponse<GetCharacterDto>> AddWeapon(AddWeaponDto newWeapon)
+		public async Task<ServiceResponse<GetCharacterDto>> AddWeapon(AddWeaponDto newWeapon)
 		{
-			throw new NotImplementedException();
+			var response = new ServiceResponse<GetCharacterDto>();
+
+			try
+			{
+				var character = await _context.Characters
+										.FirstOrDefaultAsync(c => c.Id == newWeapon.CharacterId &&
+										c.User!.Id == int.Parse(_httpContextAccessor.HttpContext!.User.FindFirstValue(ClaimTypes.NameIdentifier)!));
+
+				if (character is null)
+				{
+					response.Success = false;
+					response.Message = "Character not found.";
+					return response;
+				}
+
+				var weapon = new Weapon
+				{
+					Name = newWeapon.Name,
+					Damage = newWeapon.Damage,
+					Character = character
+				};
+
+				_context.Weapons.Add(weapon);
+				await _context.SaveChangesAsync();
+
+				response.Data = _mapper.Map<GetCharacterDto>(character);
+			}
+			catch (Exception ex)
+			{
+				response.Success = false;
+				response.Message = ex.Message;
+			}
+
+			return response;
 		}
 	}
 }
