1c40d5bb088f2e083a508b0fa3c47d8f6abf8512
rezwan <rezwanshahriorrahin@gmail.com>
2024-02-15 15:55:18 +0600

    added a method named 'AddCharacterSkill' to 'ICharacterService' interface

--- RPG/Services/CharacterService/ICharacterService.cs
+++ RPG/Services/CharacterService/ICharacterService.cs
@@ -10,5 +10,6 @@ namespace RPG.Services.CharacterService
 		Task<ServiceResponse<List<GetCharacterDto>>> AddCharacter(AddCharacterDto newCharacter);
 		Task<ServiceResponse<GetCharacterDto>> UpdateCharacter(UpdateCharacterDto updatedCharacter);
 		Task<ServiceResponse<List<GetCharacterDto>>> DeleteCharacter(int id);
+		Task<ServiceResponse<GetCharacterDto>> AddCharacterSkill(AddCharacterSkillDto newCharacterSkill);
 	}
 }
