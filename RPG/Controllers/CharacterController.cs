ac177ff7ccb4b2487896c995641d2b82e75d7076
rezwan <rezwanshahriorrahin@gmail.com>
2024-02-15 16:04:38 +0600

    added a HttpPost action method named 'AddCharacterSkill' to 'CharacterController'

--- RPG/Controllers/CharacterController.cs
+++ RPG/Controllers/CharacterController.cs
@@ -58,5 +58,11 @@ namespace RPG.Controllers
 			}
 			return Ok(response);
 		}
+
+		[HttpPost("Skill")]
+		public async Task<ActionResult<ServiceResponse<GetCharacterDto>>> AddCharacterSkill(AddCharacterSkillDto newCharacterSkill)
+		{
+			return Ok(await _characterService.AddCharacterSkill(newCharacterSkill));
+		}
 	}
 }
