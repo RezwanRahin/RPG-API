88f57b0d6ae9e722437623e5811e98961e9b927c
rezwan <rezwanshahriorrahin@gmail.com>
2024-02-16 20:45:02 +0600

    added a 'DbSet' property of 'Skill' to 'DbContext' class

--- RPG/Data/DataContext.cs
+++ RPG/Data/DataContext.cs
@@ -13,5 +13,6 @@ namespace RPG.Data
 		public DbSet<Character> Characters => Set<Character>();
 		public DbSet<User> Users => Set<User>();
 		public DbSet<Weapon> Weapons => Set<Weapon>();
+		public DbSet<Skill> Skills => Set<Skill>();
 	}
 }
