a4cff756f59d87b3fc11efb6db763d1cda6ff8bb
rezwan <rezwanshahriorrahin@gmail.com>
2024-02-19 20:17:42 +0600

    added 3 properties to 'Character' model class

--- RPG/Models/Character.cs
+++ RPG/Models/Character.cs
@@ -12,5 +12,8 @@
 		public User? User { get; set; }
 		public Weapon? Weapon { get; set; }
 		public List<Skill>? Skills { get; set; }
+		public int Fights { get; set; }
+		public int Victories { get; set; }
+		public int Defeats { get; set; }
 	}
 }
