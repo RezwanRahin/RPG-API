12140dd5bfc67239636f2e6fd8ca790fc5bc2ed8
rezwan <rezwanshahriorrahin@gmail.com>
2024-02-20 23:03:35 +0600

    added a constructor, injecting 'DataContext', to 'FightService' class

--- RPG/Services/FightService/FightService.cs
+++ RPG/Services/FightService/FightService.cs
@@ -1,10 +1,18 @@
-﻿using RPG.Dtos.Fight;
+﻿using RPG.Data;
+using RPG.Dtos.Fight;
 using RPG.Models;
 
 namespace RPG.Services.FightService
 {
 	public class FightService : IFightService
 	{
+		private readonly DataContext _context;
+
+		public FightService(DataContext context)
+		{
+			_context = context;
+		}
+
 		public Task<ServiceResponse<AttackResultDto>> WeaponAttack(WeaponAttackDto request)
 		{
 			throw new NotImplementedException();
