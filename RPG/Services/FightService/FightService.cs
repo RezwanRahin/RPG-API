c293546034cce50c526f2df4c2865b9e77a7548a
rezwan <rezwanshahriorrahin@gmail.com>
2024-02-21 20:39:05 +0600

    added a method named 'WeaponAttack' to 'FightService' class

--- RPG/Services/FightService/FightService.cs
+++ RPG/Services/FightService/FightService.cs
@@ -1,4 +1,5 @@
-﻿using RPG.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using RPG.Data;
 using RPG.Dtos.Fight;
 using RPG.Models;
 
@@ -13,9 +14,50 @@ namespace RPG.Services.FightService
 			_context = context;
 		}
 
-		public Task<ServiceResponse<AttackResultDto>> WeaponAttack(WeaponAttackDto request)
+		public async Task<ServiceResponse<AttackResultDto>> WeaponAttack(WeaponAttackDto request)
 		{
-			throw new NotImplementedException();
+			var response = new ServiceResponse<AttackResultDto>();
+
+			try
+			{
+				var attacker = await _context.Characters
+										.Include(c => c.Weapon)
+										.FirstOrDefaultAsync(c => c.Id == request.AttackerId);
+
+				var opponent = await _context.Characters.FirstOrDefaultAsync(c => c.Id == request.OpponentId);
+
+				if (attacker is null || opponent is null || attacker.Weapon is null)
+					throw new Exception("Something fishy is going on here...");
+
+
+				int damage = attacker.Weapon.Damage + new Random().Next(attacker.Strength);
+				damage -= new Random().Next(opponent.Defense);
+
+				if (damage > 0)
+					opponent.HitPoints -= damage;
+
+				if (opponent.HitPoints <= 0)
+					response.Message = $"{opponent.Name} has been defeated!";
+
+
+				await _context.SaveChangesAsync();
+
+				response.Data = new AttackResultDto
+				{
+					Attacker = attacker.Name,
+					Opponent = opponent.Name,
+					AttackerHP = attacker.HitPoints,
+					OpponentHP = opponent.HitPoints,
+					Damage = damage
+				};
+			}
+			catch (Exception ex)
+			{
+				response.Success = false;
+				response.Message = ex.Message;
+			}
+
+			return response;
 		}
 	}
 }
