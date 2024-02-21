985484006f5d800cc3a274bb4bafb743069f588f
rezwan <rezwanshahriorrahin@gmail.com>
2024-02-22 00:42:38 +0600

    created a controller named 'FightController'

---
+++ RPG/Controllers/FightController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RPG.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class FightController : ControllerBase
+	{
+	}
+}
