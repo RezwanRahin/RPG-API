c73a33daa9b5d89407f5205fd0a7a0976cf8e54c
rezwan <rezwanshahriorrahin@gmail.com>
2024-02-27 14:03:04 +0600

    created a service interface named 'IFightService'

---
+++ RPG/Services/FightService/IFightService.cs
@@ -0,0 +1,10 @@
+﻿using RPG.Dtos.Fight;
+using RPG.Models;
+
+namespace RPG.Services.FightService
+{
+	public interface IFightService
+	{
+		Task<ServiceResponse<AttackResultDto>> WeaponAttack(WeaponAttackDto request);
+	}
+}
