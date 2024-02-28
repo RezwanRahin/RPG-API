61fb8a5e3b86292f1d9e614da28d45f8fd88c655
rezwan <rezwanshahriorrahin@gmail.com>
2024-02-29 00:00:14 +0600

    created a class, implementing its interface, named 'FightService'

---
+++ RPG/Services/FightService/FightService.cs
@@ -0,0 +1,13 @@
+﻿using RPG.Dtos.Fight;
+using RPG.Models;
+
+namespace RPG.Services.FightService
+{
+	public class FightService : IFightService
+	{
+		public Task<ServiceResponse<AttackResultDto>> WeaponAttack(WeaponAttackDto request)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
