4077927b6728cac897cc08df624af61418547182
rezwan <rezwanshahriorrahin@gmail.com>
2024-03-05 20:43:55 +0600

    added a method named 'SkillAttack' to 'IFightService' class

--- RPG/Services/FightService/IFightService.cs
+++ RPG/Services/FightService/IFightService.cs
@@ -6,5 +6,6 @@ namespace RPG.Services.FightService
 	public interface IFightService
 	{
 		Task<ServiceResponse<AttackResultDto>> WeaponAttack(WeaponAttackDto request);
+		Task<ServiceResponse<AttackResultDto>> SkillAttack(SkillAttackDto request);
 	}
 }
