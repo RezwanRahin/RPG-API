b989d5cc5924bf76073d1516dc259813e2bb2794
rezwan <rezwanshahriorrahin@gmail.com>
2024-03-07 23:32:22 +0600

    added a HttpPost action method named 'SkillAttack' to 'FightController'

--- RPG/Controllers/FightController.cs
+++ RPG/Controllers/FightController.cs
@@ -21,5 +21,11 @@ namespace RPG.Controllers
 		{
 			return Ok(await _fightService.WeaponAttack(request));
 		}
+
+		[HttpPost("Skill")]
+		public async Task<ActionResult<ServiceResponse<AttackResultDto>>> SkillAttack(SkillAttackDto request)
+		{
+			return Ok(await _fightService.SkillAttack(request));
+		}
 	}
 }
