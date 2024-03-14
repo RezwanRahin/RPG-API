b1403fa4a69ba3751c6c711f1e843d2b5fb4cc33
rezwan <rezwanshahriorrahin@gmail.com>
2024-03-14 22:19:08 +0600

    added implemented method named 'Fight' to 'FightService' class

--- RPG/Services/FightService/FightService.cs
+++ RPG/Services/FightService/FightService.cs
@@ -14,6 +14,82 @@ namespace RPG.Services.FightService
 			_context = context;
 		}
 
+		public async Task<ServiceResponse<FightResultDto>> Fight(FightRequestDto request)
+		{
+			var response = new ServiceResponse<FightResultDto>
+			{
+				Data = new FightResultDto()
+			};
+
+			try
+			{
+				var characters = await _context.Characters
+										.Include(c => c.Weapon)
+										.Include(c => c.Skills)
+										.Where(c => request.CharacterIds.Contains(c.Id))
+										.ToListAsync();
+
+				bool defeated = false;
+
+				while (!defeated)
+				{
+					foreach (var attacker in characters)
+					{
+						var opponents = characters.Where(c => c.Id != attacker.Id).ToList();
+						var opponent = opponents[new Random().Next(opponents.Count)];
+
+						int damage = 0;
+						string attackUsed = string.Empty;
+						bool useWeapon = new Random().Next(2) == 0;
+
+						if (useWeapon && attacker.Weapon is not null)
+						{
+							attackUsed = attacker.Weapon.Name;
+							damage = DoWeaponAttack(attacker, opponent);
+						}
+						else if (!useWeapon && attacker.Skills is not null)
+						{
+							var skill = attacker.Skills[new Random().Next(attacker.Skills.Count)];
+							attackUsed = skill.Name;
+							damage = DoSkillAttack(attacker, opponent, skill);
+						}
+						else
+						{
+							response.Data.Log.Add($"{attacker.Name} wasn't able to attack!");
+							continue;
+						}
+
+						response.Data.Log.Add($"{attacker.Name} attacks {opponent.Name} using {attackUsed} with {(damage >= 0 ? damage : 0)} damage");
+
+						if (opponent.HitPoints <= 0)
+						{
+							defeated = true;
+							attacker.Victories++;
+							opponent.Defeats++;
+							response.Data.Log.Add($"{opponent.Name} has been defeated!");
+							response.Data.Log.Add($"{attacker.Name} wins with {attacker.HitPoints} HP left!");
+							break;
+						}
+					}
+				}
+
+				characters.ForEach(c =>
+				{
+					c.Fights++;
+					c.HitPoints = 100;
+				});
+
+				await _context.SaveChangesAsync();
+			}
+			catch (Exception ex)
+			{
+				response.Success = false;
+				response.Message = ex.Message;
+			}
+
+			return response;
+		}
+
 		public async Task<ServiceResponse<AttackResultDto>> SkillAttack(SkillAttackDto request)
 		{
 			var response = new ServiceResponse<AttackResultDto>();
